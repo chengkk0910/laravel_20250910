a07d26a504e76e5d935156b071c29e397c00a13f
kai <cgk0910@gmail.com>
2025-09-17 14:29:49 +0800

    get data

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Student;
 
 
 class StudentController extends Controller
@@ -13,7 +14,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-
+        $data = Student::all();
+        dd($data);
         return view('student.index');
     }
 
